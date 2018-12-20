7b88939ab9cd3402249d59c9c97e42b7ea3984eb
MaryamOsamaX <MaryamOsamaX@users.noreply.github.com>
2018-12-20 22:21:58 +0300

    Models
    Models

---
+++ src/main/java/fci/software/project/item/Item.java
@@ -0,0 +1,48 @@
+package fci.software.project.item;
+
+import javax.persistence.*;
+@Entity
+public class Item {
+	private String itemName , type ,color ,userId , itemId;
+
+	public String getItemName() {
+		return itemName;
+	}
+
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+   @Id
+	public String getItemId() {
+		return itemId;
+	}
+
+	public void setItemId(String itemId) {
+		this.itemId = itemId;
+	}
+
+}
